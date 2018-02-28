d21753bffb915b5de2d918aaa107d10cc2b062dc
Unknown <johankjellin93@gmail.com>
2018-02-28 12:24:14 +0100

    change the type and zoom level

--- Exercise Files/Ch05/05_02/script.js
+++ Exercise Files/Ch05/05_02/script.js
@@ -4,10 +4,13 @@ var map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('hplus-map'), {
-        center: {
+        'center': {
             lat: 34.1031131,
             lng: -118.326343
         },
-        zoom: 8
+        'zoom': 12,
+        'mapTypeId': google.maps.MapTypeId.ROADMAP,
+        'draggable': false,
+        'scroolwheel': false
     });
 }
