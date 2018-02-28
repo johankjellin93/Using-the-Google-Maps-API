34f1f6f32ed665044034c683bea83310a75f71b3
Unknown <johankjellin93@gmail.com>
2018-02-28 12:47:18 +0100

    challenge

--- Exercise Files/Ch05/05_02/script.js
+++ Exercise Files/Ch05/05_02/script.js
@@ -4,10 +4,11 @@ var map;
 
 function initMap() {
     var storeLocation = {lat: 34.1031131, lng: -118.326343};
+    var secondStoreLocation = {lat: 34.074541, lng: -118.240888}
 
     map = new google.maps.Map(document.getElementById('hplus-map'), {
         'center': storeLocation,
-        'zoom': 12,
+        'zoom': 11,
         'mapTypeId': google.maps.MapTypeId.ROADMAP,
         'draggable': false,
         'scroolwheel': false
@@ -17,8 +18,16 @@ function initMap() {
         popupContent += '1750 Vine St<br>';
         popupContent += 'Los Angeles, CA<br>';
 
+    var secondPopupContent = 'H+ Sport<br>';
+        secondPopupContent += '1000 Vin Scully Ave<br>'
+        secondPopupContent += 'Los Angeles, CA'
+
     var infowindow = new google.maps.InfoWindow({
         'content': popupContent
+    });
+
+    var secondInfoWindow = new google.maps.InfoWindow({
+        'content': secondPopupContent
     });
 
     var marker = new google.maps.Marker({
@@ -26,9 +35,19 @@ function initMap() {
         'map': map,
         'title': 'hplus sport in Los Angeles'
     });
-
     marker.addListener('click', function() {
         infowindow.open(map, marker)
     });
+
+    var secondMarker = new google.maps.Marker({
+        'position': secondStoreLocation,
+        'map': map,
+        'title': 'hplus sport in Los Angeles'
+    });
+    secondMarker.addListener('click', function() {
+        secondInfoWindow.open(map, secondMarker)
+    });
+
+    
 
 }
