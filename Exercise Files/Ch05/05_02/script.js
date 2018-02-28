d4f04509abff0a4fcf72e3217c566d1f1381a6a1
Unknown <johankjellin93@gmail.com>
2018-02-28 12:12:29 +0100

    change the center point

--- Exercise Files/Ch05/05_02/script.js
+++ Exercise Files/Ch05/05_02/script.js
@@ -5,8 +5,8 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('hplus-map'), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 34.1031131,
+            lng: -118.326343
         },
         zoom: 8
     });
