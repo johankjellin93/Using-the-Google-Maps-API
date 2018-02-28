a2963413419e40759cc18dcacac42f3a00dbb003
Unknown <johankjellin93@gmail.com>
2018-02-28 12:57:33 +0100

    solution

--- Exercise Files/Ch05/05_02/script.js
+++ Exercise Files/Ch05/05_02/script.js
@@ -5,10 +5,11 @@ var map;
 function initMap() {
     var storeLocation = {lat: 34.1031131, lng: -118.326343};
     var secondStoreLocation = {lat: 34.074541, lng: -118.240888}
+    var centerPoint = {lat: 34.0937772394951, lng: -118.27888622568359}
 
     map = new google.maps.Map(document.getElementById('hplus-map'), {
-        'center': storeLocation,
-        'zoom': 11,
+        'center': centerPoint,
+        'zoom': 12,
         'mapTypeId': google.maps.MapTypeId.ROADMAP,
         'draggable': false,
         'scroolwheel': false
@@ -48,6 +49,16 @@ function initMap() {
         secondInfoWindow.open(map, secondMarker)
     });
 
-    
+}
 
+function geoCodeAddres(address) {
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            console.log('Geocode address: ', results[0].geometry.location.tostring());
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
 }
