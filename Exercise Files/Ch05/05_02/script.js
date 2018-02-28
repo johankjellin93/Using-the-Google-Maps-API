608518f982eeb78a96040e5fb7cb9a8589269ca0
Unknown <johankjellin93@gmail.com>
2018-02-28 12:29:49 +0100

    add a marker

--- Exercise Files/Ch05/05_02/script.js
+++ Exercise Files/Ch05/05_02/script.js
@@ -3,14 +3,20 @@ var gMapAPIKey = 'AIzaSyAwMlqTLt3ryh3irRjUWWfRWfsoCbjRRQw'
 var map;
 
 function initMap() {
+    var storeLocation = {lat: 34.1031131, lng: -118.326343};
+
     map = new google.maps.Map(document.getElementById('hplus-map'), {
-        'center': {
-            lat: 34.1031131,
-            lng: -118.326343
-        },
+        'center': storeLocation,
         'zoom': 12,
         'mapTypeId': google.maps.MapTypeId.ROADMAP,
         'draggable': false,
         'scroolwheel': false
     });
+
+    var marker = new google.maps.Marker({
+        'position': storeLocation,
+        'map': map,
+        'title': 'hplus sport in Los Angeles'
+    });
+
 }
